fab96bf53c673cfdca4e12933b63ba27dac34e4d
Jaime <garciadiazjaime@gmail.com>
2025-08-24 14:56:38 -0500

    feat: gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
+
 import "./globals.css";
 
 import Menu from '@/app/shared/components/Menu';
@@ -33,6 +35,7 @@ export default function RootLayout({
           padding: '16px',
           backgroundColor: '#f9f9f9',
         }}>
+          <GoogleTagManager gtmId="G-8PLPTP51Z6" />
           <Menu />
           {children}
         </div>
